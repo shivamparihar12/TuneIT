55bc1ae584716fd7b3d5e8d307aa2d921d004f79
shivamparihar12 <parihar.shivam0789@gmail.com>
2021-10-28 15:27:45 +0530

    media controls

---
+++ app/src/main/java/com/example/tuneit/MediaPlaybackService.kt
@@ -0,0 +1,69 @@
+package com.example.tuneit
+
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.media.MediaBrowserServiceCompat
+
+private const val MY_MEDIA_ROOT_ID = "media_root_id"
+private const val MY_EMPTY_MEDIA_ROOT_ID = "empty_root_id"
+
+class MediaPlayerService : MediaBrowserServiceCompat() {
+
+    private var mediaSession: MediaSessionCompat? = null
+    private lateinit var stateBuilder: PlaybackStateCompat.Builder
+    private val LOG_TAG = "MediaPlayerService"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        mediaSession = MediaSessionCompat(baseContext, LOG_TAG).apply {
+            setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS
+                        or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
+            )
+        }
+
+        stateBuilder = PlaybackStateCompat.Builder()
+            .setActions(
+                PlaybackStateCompat.ACTION_PAUSE
+                        or PlaybackStateCompat.ACTION_PLAY_PAUSE
+            )
+        setPlaybackState(stateBuilder.build())
+
+        setCallback(MySessionCallback)
+        setSessionToken(sessionToken)
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        return if (allowBrowsing(clientPackageName, clientUid)) {
+            MediaBrowserServiceCompat.BrowserRoot(MY_MEDIA_ROOT_ID, null)
+        } else {
+            MediaBrowserServiceCompat.BrowserRoot(MY_EMPTY_MEDIA_ROOT_ID, null)
+        }
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        if (MY_EMPTY_MEDIA_ROOT_ID==parentId){
+            result.sendResult(null)
+        }
+
+        val mediaItems = emptyList<MediaBrowserServiceCompat>()
+
+        if (MY_MEDIA_ROOT_ID==parentId){
+
+        }
+        else{
+
+        }
+        //result.sendResult(mediaItems)
+    }
+}
